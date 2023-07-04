cc25605ea9748a73738b7d96b8667552f65a2618
toxoxi <yorori.too@gmail.com>
2023-07-05 08:43:08 +0900

    introduce "depth" to Content object and put a ceiling on the maximum depth

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,37 +1,43 @@
-import { BuiltInPowerupCodes, RNPlugin, Rem } from "@remnote/plugin-sdk";
+import { BuiltInPowerupCodes, RNPlugin, Rem, RemId } from "@remnote/plugin-sdk";
 
 type Content = {
-  [remId: string]: {
+  [remId: RemId]: {
+    depth: number;
     text: string;
     children: Content[];
   }
 }
 
+const MAX_DEPTH = 4 as const;
+
 export async function generateContents(root: Rem, plugin: RNPlugin): Promise<Content[]> {
   const childrenRem = await root.getChildrenRem();
-  const contents = await buildChildren(childrenRem, plugin);
+  const contents = await buildChildren(1, childrenRem, plugin);
   return contents;
 }
 
-async function buildChildren(childrenRem: Rem[], plugin: RNPlugin): Promise<Content[]> {
+async function buildChildren(depth: number, childrenRem: Rem[], plugin: RNPlugin): Promise<Content[]> {
+  if (depth > MAX_DEPTH) { return []; }
+
   const children = await Promise.all(
     childrenRem.map(async (child) => {
       const isHeader = await isHeaderRem(child);
       if (!isHeader) { return {}; }
     
-      return buildContent(child, plugin);
+      return buildContent(depth, child, plugin);
     })
   )
 
   return children.filter(child => Object.keys(child).length !== 0);
 }
 
-async function buildContent(rem: Rem, plugin: RNPlugin): Promise<Content> {
+async function buildContent(depth: number, rem: Rem, plugin: RNPlugin): Promise<Content> {
   const [id, text, childrenRem] = await extractAttributes(rem, plugin)
-  const children = await buildChildren(childrenRem, plugin);
+  const children = await buildChildren(depth+1, childrenRem, plugin);
 
   const content = {
     [id]: {
+      depth,
       text,
       children,
     }
